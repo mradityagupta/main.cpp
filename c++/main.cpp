4c1412fff18cc6a13262d2cb052be790669b73dc
mradityagupta <mr.adityagupta11@gmail.com>
2024-11-16 22:10:03 +0530

    Add existing project files to git

---
+++ c++/main.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a;
+    // cout<<"Enter a (a) number: ";
+    // cin>>a;
+    // int b;
+    // cout<<"Enter a (b) number: ";
+    // cin>>b;
+    // cout<<"Sum of (a+b) number: "<<a+b;
+
+    // int package;
+    // cout<<"Enter a package:";
+    // cin>>package;
+    // if(package>10)
+    // {
+    //     cout<<"Accepted";
+    // }
+    // else{
+    //     cout<<"Not Accepted";
+    // }
+
+    // int marks;
+    // cout<<"Enter a marks:";
+    // cin>>marks;
+    // if(marks>33)
+    // {
+    //     cout<<"Pass";
+    // }
+    // else
+    // {
+    //     cout<<"Fail";
+    // }
+
+    // int a;
+    // cout<<"Enter your first number:";
+    // cin>>a;
+    // int b;
+    // cout<<"Enter your Second number:";
+    // cin>>b;
+    // if(a>b)
+    // {
+    //     cout<<"Yes";
+    // }
+    // else
+    // {
+    //     cout<<"No";
+    // }
+
+    // int number;
+    // cout<<"Enter your number:";
+    // cin>>number;
+    // if(number%2==0)
+    // {
+    //     cout<<"Even";
+    // }
+    // else
+    // {
+    //     cout<<"Odd";
+    // }
+
+    // int age;
+    // cout<<"Enter a age:";
+    // cin>>age;
+    // if(age<18)
+    // {
+    //     cout<<"Boy And Girl";
+    // }
+    // else
+    // {
+    //     cout<<"Men And Women";
+    // }
+
+    // int number;
+    // cout<<"Enter a number:";
+    // cin>>number;
+    // if(number>0)
+    // {
+    //     cout<<"Positive";
+    // }
+    // else if(number==0)
+    // {
+    //     cout<<"0";
+    // }
+    // else
+    // {
+    //     cout<<"Negative";
+    // }
+
+    int vowel;
+    cout<<"Enter a vowel:";
+    cin>>vowel;
+    int consonent;
+    cout<<"Enter a consonent:";
+    cin>>consonent;
+    if()
+
+
+    return 0;
+}
